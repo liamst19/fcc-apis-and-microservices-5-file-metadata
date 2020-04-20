f1cbe9c32995af3dd3b144d07dc741f863827079
Glitch (heavy-glittery-metal) <none>
2020-04-20 10:10:41 +0000

    🌦🎊 Checkpoint ./package.json:15054522/947 ./server.js:15054522/428

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+const multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `upfile` file
+  
+})
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
